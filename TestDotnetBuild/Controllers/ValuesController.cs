3389270f9c801f2ff5a66183c45c43672ecb1a25
Duy Tran <dtran@seic.com>
2017-10-30 10:50:17 -0400

    initialize test dotnet build project

--- TestDotnetBuild/Controllers/ValuesController.cs
+++ TestDotnetBuild/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace TestDotnetBuild.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "dtran", "duy tran" };
         }
 
         // GET api/values/5
